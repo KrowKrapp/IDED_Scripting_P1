1ba0370ce8dd60972e640c6181514968f1f4765c
Kottar <valeria.quinteroc@upb.edu.co>
2025-05-13 21:40:50 -0500

    commit1

--- Sandbox.cs
+++ Sandbox.cs
@@ -6,8 +6,51 @@ namespace TestProject1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Here you can write console prints to test your implementation outside the testing environment");
+            using System;
+            using System.Collections.Generic;
+
+namespace TestProject1
+    {
+        internal class Sandbox
+        {
+            public static void Main(string[] args)
+            {
+                Stack<int> pila = new Stack<int>();
+                pila.Push(18);
+                pila.Push(27);
+                pila.Push(17); 
+                pila.Push(45);
+
+                Console.WriteLine("Probando GetFirstPrime :P");
+                int primerPrimo = TestMethods.GetFirstPrime(pila);
+                Console.WriteLine($"Primer número primo encontrado: {primerPrimo}"); 
+
+                Console.WriteLine("Probando RemoveFirstPrime");
+                TestMethods.RemoveFirstPrime(pila);
+                Console.WriteLine("Pila después de eliminar el primer primo:");
+                foreach (int num in pila)
+                    Console.WriteLine(num);
+
+                Console.WriteLine("Probando StackToQueue");
+                Queue<int> cola = TestMethods.StackToQueue(pila);
+                Console.WriteLine("Cola generada desde la pila:");
+                foreach (int num in cola)
+                    Console.WriteLine(num);
+
+                Console.WriteLine("Probando StackToList");
+                List<int> lista = TestMethods.StackToList(pila);
+                Console.WriteLine("Lista generada desde la pila:");
+                foreach (int num in lista)
+                    Console.WriteLine(num);
+
+                Console.WriteLine("Probando IsNumberInSortedList");
+                List<int> lista2 = new List<int> { 42, 13, 7, 66, 1, 3 };
+                int target = 7;
+                bool encontrado = TestMethods.IsNumberInSortedList(lista2, target);
+                Console.WriteLine($"¿El número {target} está en la lista? {encontrado}");
+            }
         }
+    }
+}
     }
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -6,27 +7,136 @@ namespace TestProject1
     {
         internal static uint StackFirstPrime(Stack<uint> stack)
         {
-            return 0;
+            Stack<uint> temp = new Stack<uint>();
+            uint result = 0;
+
+            while (stack.Count > 0)
+            {
+                uint value = stack.Pop();
+                temp.Push(value);
+                if (result == 0 && IsPrime(value))
+                {
+                    result = value;
+                }
+            }
+
+            while (temp.Count > 0)
+            {
+                stack.Push(temp.Pop());
+            }
+
+            return result;
         }
 
         internal static Stack<uint> RemoveFirstPrime(Stack<uint> stack)
         {
-            return null;
+            Stack<uint> reversed = new Stack<uint>();
+            bool removed = false;
+
+            while (stack.Count > 0)
+            {
+                uint val = stack.Pop();
+                if (!removed && IsPrime(val))
+                {
+                    removed = true;
+                    continue;
+                }
+                reversed.Push(val);
+            }
+
+            Stack<uint> result = new Stack<uint>();
+            while (reversed.Count > 0)
+            {
+                result.Push(reversed.Pop());
+            }
+
+            return result;
         }
 
         internal static Queue<uint> CreateQueueFromStack(Stack<uint> stack)
         {
-            return null;
+            Stack<uint> temp = new Stack<uint>();
+            Queue<uint> queue = new Queue<uint>();
+
+            while (stack.Count > 0)
+            {
+                temp.Push(stack.Pop());
+            }
+
+            while (temp.Count > 0)
+            {
+                uint val = temp.Pop();
+                stack.Push(val); 
+                queue.Enqueue(val);
+            }
+
+            return queue;
         }
 
         internal static List<uint> StackToList(Stack<uint> stack)
         {
-            return null;
+            Stack<uint> temp = new Stack<uint>();
+            List<uint> list = new List<uint>();
+
+            while (stack.Count > 0)
+            {
+                temp.Push(stack.Pop());
+            }
+
+            while (temp.Count > 0)
+            {
+                uint val = temp.Pop();
+                list.Add(val);
+                stack.Push(val); 
+            }
+
+            return list;
         }
+
 
         internal static bool FoundElementAfterSorted(List<int> list, int value)
         {
+            int n = list.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+
+            int left = 0, right = list.Count - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (list[mid] == value)
+                    return true;
+                else if (list[mid] < value)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
             return false;
+        }
+
+        private static bool IsPrime(uint number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+
+            for (uint i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
